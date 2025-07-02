09400e5be2c3c595f7f89de3fe828c1f26b8f90d
Anderson Tu <ziziandydy@gmail.com>
2025-07-02 16:45:42 +0800

    chore: Web Pixel Extension 加入 console.log 以確認載入與事件觸發

--- extensions/ddkt-tracjing/src/index.ts
+++ extensions/ddkt-tracjing/src/index.ts
@@ -1,28 +1,35 @@
 import { register } from "@shopify/web-pixels-extension";
 
 register(({ analytics }) => {
+  console.log('[Pixel Extension] register called');
+
   // Page view
   analytics.subscribe('page_viewed', (event) => {
+    console.log('[Pixel Extension] page_viewed', event);
     sendToServer('page_viewed', event);
   });
 
   // Product view
   analytics.subscribe('product_viewed', (event) => {
+    console.log('[Pixel Extension] product_viewed', event);
     sendToServer('product_viewed', event);
   });
 
   // Cart updated
   analytics.subscribe('cart_updated', (event) => {
+    console.log('[Pixel Extension] cart_updated', event);
     sendToServer('cart_updated', event);
   });
 
   // Checkout started
   analytics.subscribe('checkout_started', (event) => {
+    console.log('[Pixel Extension] checkout_started', event);
     sendToServer('checkout_started', event);
   });
 
   // Checkout completed
   analytics.subscribe('checkout_completed', (event) => {
+    console.log('[Pixel Extension] checkout_completed', event);
     sendToServer('checkout_completed', event);
   });
 
